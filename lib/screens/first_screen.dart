e80ad062b83b37ecc6d6e45fbdc2ba8f9c918972
honestyman <leel50296@gmail.com>
2024-11-20 01:39:43 +0900

    update first screen

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -43,176 +43,6 @@ class _FirstScreenState extends State<FirstScreen> {
     }
   }
 
-  Future<void> lineLogin() async {
-    try {
-      final result = await LineSDK.instance.login();
-      print("rrrrrr  " + result.toString());
-      if (result != null) {
-        final isLogin = await Provider.of<UserState>(context, listen: false)
-            .loginWithLine(
-                result.userProfile!.userId, result.userProfile!.displayName);
-        if (isLogin) {
-          Navigator.pushNamed(context, "/loginhome");
-        } else {
-          showDialog(
-              barrierDismissible: false,
-              context: context,
-              builder: (_) => Center(
-                      // Aligns the container to center
-                      child: Container(
-                    // A simplified version of dialog.
-                    width: 300,
-                    height: 150,
-                    padding: const EdgeInsets.only(top: 35),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: AppColors.primaryWhite),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "LINEログインに失敗しました。",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: AppColors.primaryBlack,
-                              fontWeight: FontWeight.normal,
-                              fontSize: 15,
-                              letterSpacing: -1,
-                              decoration: TextDecoration.none),
-                        ),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          child: Container(
-                            width: 343,
-                            height: 42,
-                            margin: const EdgeInsets.only(top: 5),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    top: BorderSide(
-                                        color: AppColors.secondaryGray
-                                            .withOpacity(0.5)))),
-                            child: MaterialButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: Center(
-                                child: CustomText(
-                                    text: "OK",
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.normal,
-                                    lineHeight: 1,
-                                    letterSpacing: -1,
-                                    color: AppColors.alertBlue),
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  )));
-        }
-      }
-    } on PlatformException catch (e) {
-      print(e.message);
-    }
-  }
-
-  Future<void> appleLogin() async {
-    try {
-      final credential  = await SignInWithApple.getAppleIDCredential(
-        scopes: [
-          AppleIDAuthorizationScopes.email,
-          AppleIDAuthorizationScopes.fullName,
-        ],
-      );
-      if (credential != null && credential.email != "") {
-        final isLogin = await Provider.of<UserState>(context, listen: false).loginWithApple(credential.email!);
-          if (isLogin) {
-            Navigator.pushNamed(context, "/loginhome");
-          } else {
-            showDialog(
-                barrierDismissible: false,
-                context: context,
-                builder: (_) => Center(
-                    // Aligns the container to center
-                    child: Container(
-                  // A simplified version of dialog.
-                  width: 300,
-                  height: 150,
-                  padding:
-                      const EdgeInsets.only(top: 35),
-                  decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.circular(10),
-                      color: AppColors.primaryWhite),
-                  child: Column(
-                    crossAxisAlignment:
-                        CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "AppleIDログインに失敗しました。",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color:
-                                AppColors.primaryBlack,
-                            fontWeight:
-                                FontWeight.normal,
-                            fontSize: 15,
-                            letterSpacing: -1,
-                            decoration:
-                                TextDecoration.none),
-                      ),
-                      const SizedBox(
-                        height: 24,
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.symmetric(
-                                horizontal: 10),
-                        child: Container(
-                          width: 343,
-                          height: 42,
-                          margin: const EdgeInsets.only(
-                              top: 5),
-                          decoration: BoxDecoration(
-                              border: Border(
-                                  top: BorderSide(
-                                      color: AppColors
-                                          .secondaryGray
-                                          .withOpacity(
-                                              0.5)))),
-                          child: MaterialButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Center(
-                              child: CustomText(
-                                  text: "OK",
-                                  fontSize: 15,
-                                  fontWeight:
-                                      FontWeight.normal,
-                                  lineHeight: 1,
-                                  letterSpacing: -1,
-                                  color: AppColors
-                                      .alertBlue),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                )));
-          }
-      }
-      print(credential);
-    } catch (e) {
-      print(e);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     // ignore: deprecated_member_use
@@ -373,7 +203,96 @@ class _FirstScreenState extends State<FirstScreen> {
                           // borderColor: AppColors.primaryBlue,
                           titleColor: AppColors.primaryWhite,
                           onTap: () async{
-                            await appleLogin();
+                            try {
+                              final credential  = await SignInWithApple.getAppleIDCredential(
+                                scopes: [
+                                  AppleIDAuthorizationScopes.email,
+                                  AppleIDAuthorizationScopes.fullName,
+                                ],
+                              );
+                              if (credential != null && credential.email != "") {
+                                final isLogin = await Provider.of<UserState>(context, listen: false).loginWithApple(credential.email!);
+                                  if (isLogin) {
+                                    Navigator.pushNamed(context, "/loginhome");
+                                  } else {
+                                    showDialog(
+                                        barrierDismissible: false,
+                                        context: context,
+                                        builder: (_) => Center(
+                                            // Aligns the container to center
+                                            child: Container(
+                                          // A simplified version of dialog.
+                                          width: 300,
+                                          height: 150,
+                                          padding:
+                                              const EdgeInsets.only(top: 35),
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              color: AppColors.primaryWhite),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                "AppleIDログインに失敗しました。",
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                    color:
+                                                        AppColors.primaryBlack,
+                                                    fontWeight:
+                                                        FontWeight.normal,
+                                                    fontSize: 15,
+                                                    letterSpacing: -1,
+                                                    decoration:
+                                                        TextDecoration.none),
+                                              ),
+                                              const SizedBox(
+                                                height: 24,
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        horizontal: 10),
+                                                child: Container(
+                                                  width: 343,
+                                                  height: 42,
+                                                  margin: const EdgeInsets.only(
+                                                      top: 5),
+                                                  decoration: BoxDecoration(
+                                                      border: Border(
+                                                          top: BorderSide(
+                                                              color: AppColors
+                                                                  .secondaryGray
+                                                                  .withOpacity(
+                                                                      0.5)))),
+                                                  child: MaterialButton(
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: Center(
+                                                      child: CustomText(
+                                                          text: "OK",
+                                                          fontSize: 15,
+                                                          fontWeight:
+                                                              FontWeight.normal,
+                                                          lineHeight: 1,
+                                                          letterSpacing: -1,
+                                                          color: AppColors
+                                                              .alertBlue),
+                                                    ),
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        )));
+                                  }
+                              }
+                              print(credential);
+                            } catch (e) {
+                              print(e);
+                            }
                           }),
                     const SizedBox(
                       height: 20,
@@ -386,7 +305,80 @@ class _FirstScreenState extends State<FirstScreen> {
                         color: AppColors.primaryGreen,
                         titleColor: AppColors.primaryWhite,
                         onTap: () async{
-                          await lineLogin();
+                          try {
+                            final result = await LineSDK.instance.login();
+                            print("rrrrrr  " + result.toString());
+                            if (result != null) {
+                              final isLogin = await Provider.of<UserState>(context, listen: false)
+                                  .loginWithLine(
+                                      result.userProfile!.userId, result.userProfile!.displayName);
+                              if (isLogin) {
+                                Navigator.pushNamed(context, "/loginhome");
+                              } else {
+                                showDialog(
+                                    barrierDismissible: false,
+                                    context: context,
+                                    builder: (_) => Center(
+                                            // Aligns the container to center
+                                            child: Container(
+                                          // A simplified version of dialog.
+                                          width: 300,
+                                          height: 150,
+                                          padding: const EdgeInsets.only(top: 35),
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(10),
+                                              color: AppColors.primaryWhite),
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                "LINEログインに失敗しました。",
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                    color: AppColors.primaryBlack,
+                                                    fontWeight: FontWeight.normal,
+                                                    fontSize: 15,
+                                                    letterSpacing: -1,
+                                                    decoration: TextDecoration.none),
+                                              ),
+                                              const SizedBox(
+                                                height: 24,
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                                                child: Container(
+                                                  width: 343,
+                                                  height: 42,
+                                                  margin: const EdgeInsets.only(top: 5),
+                                                  decoration: BoxDecoration(
+                                                      border: Border(
+                                                          top: BorderSide(
+                                                              color: AppColors.secondaryGray
+                                                                  .withOpacity(0.5)))),
+                                                  child: MaterialButton(
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: Center(
+                                                      child: CustomText(
+                                                          text: "OK",
+                                                          fontSize: 15,
+                                                          fontWeight: FontWeight.normal,
+                                                          lineHeight: 1,
+                                                          letterSpacing: -1,
+                                                          color: AppColors.alertBlue),
+                                                    ),
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        )));
+                              }
+                            }
+                          } on PlatformException catch (e) {
+                            print(e.message);
+                          }
                         }),
                     // const SizedBox(
                     //   height: 20,
